63cb0aab594c023885dda724ef0c22d082eb883c
emmanuelkorir <emmanuelqkorir@gmail.com>
2023-07-19 22:26:53 +0300

    edit formhandler.inc.php

---
+++ includes/formhandler.inc.php
@@ -0,0 +1,44 @@
+<?php
+
+//check if submit button is clicked. similar to if($_SERVER['REQUEST_METHOD'] == 'POST')
+if (isset($_POST['submit'])) {
+    //get the data from the form
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    //check if the fields are empty
+    if (empty($username) || empty($email) || empty($password)) {
+        header("Location: ../index.php?error=emptyfields&username=" . $username . "&email=" . $email);
+        exit();
+    } else {
+        //catch errors
+        try {
+        //require connection file to database
+        require_once 'db.inc.php';
+
+        //prepare the sql statement
+        $query = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password);";
+
+        //prepare the sql statement for execution
+        $stmt = $pdo->prepare($query);
+        $stmt -> bindParam(':username', $username);
+        $stmt -> bindParam(':email', $email);
+        $stmt -> bindParam(':password', $password);
+
+        $stmt -> execute();
+
+        $pdo = null;
+        $stmt = null;
+
+        header("Location: ../index.php?signup=success");
+    
+
+        } catch (PDOException $e) {
+            die ("Error: " . $e->getMessage());
+        }
+    }
+} else {
+    header("Location: ../index.php");
+    exit();
+}
