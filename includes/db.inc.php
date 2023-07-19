61a56202bca4c557e804d39a3de78cfbf9ff69e5
emmanuelkorir <emmanuelqkorir@gmail.com>
2023-07-19 22:26:43 +0300

    edit dp.inc.php

---
+++ includes/db.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=tutorial";
+$dbusername = "root";
+$dbpassword = "";
+
+try {
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
