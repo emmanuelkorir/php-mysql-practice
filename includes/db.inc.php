4a433e094a60f5ebe9e7a1bde613c5df4978b25a
emmanuelkorir <emmanuelqkorir@gmail.com>
2023-07-20 15:58:00 +0300

    add db.inc.php

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -7,7 +7,7 @@ $dbpassword = "";
 try {
     $pdo = new PDO($dsn, $dbusername, $dbpassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    echo "Connected successfully"; //remove if posting results
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
