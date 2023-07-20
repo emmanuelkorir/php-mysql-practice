6aaf1470f5ec64a7113a84b2b6a4f148ae6a531c
emmanuelkorir <emmanuelqkorir@gmail.com>
2023-07-20 15:58:52 +0300

    add index.php

---
+++ phpsessions/index.php
@@ -0,0 +1,19 @@
+<?php
+    //require php session
+    require_once 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //check cookie session
+        var_dump($_COOKIE);
+    ?>
+</body>
+</html>
